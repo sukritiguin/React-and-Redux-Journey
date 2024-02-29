d53114d65320ce251a83a38edf44e702eac06258
sukritiguin <sukritiguin.ece2020@nsec.ac.in>
2024-02-29 21:11:27 +0530

    End of 001

--- 001-firstReactApp/src/App.jsx
+++ 001-firstReactApp/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import the component before use it
+import Likebutton from "./Likebutton";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// This is functional component
+function App(){
+  return  <div>
+    <h1>
+      Our First Component
+    </h1>
+    <span>
+        <Likebutton></Likebutton>
+    </span>
+  </div>
 }
 
-export default App
+export default App;
---
+++ 001-firstReactApp/src/Likebutton.jsx
@@ -0,0 +1,5 @@
+function Likebutton(){
+    return <button>Like this</button>
+}
+
+export default Likebutton;
